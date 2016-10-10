fbb0206401933a78f1ac8f01b6094db8a5aeceed
Evan Strohm <evanstrohm@gmail.com>
2016-10-10 18:14:59 -0400

    Edited 'login.py' to finally have functionality, including interaction with the database and successful output of dynamically generated HTML. Added script 'logout.py' and a button to log out a user. Note that 'logout.py' is not yet functional

---
+++ www/cgi-bin/logout.py
@@ -0,0 +1,12 @@
+#!C:\Python27\python.exe
+
+import cgitb
+import Cookie
+import os
+
+cgitb.enable()
+
+print 'Content-type: text/html'
+print 'Set-Cookie: logged_in=null; path=/; expires=Sun, 09 Oct 2016 00:00:00 GMT'
+print
+print 
