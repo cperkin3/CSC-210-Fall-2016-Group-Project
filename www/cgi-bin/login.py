9f8becd8e9b935bc66f68f6ce6789867f9f0c1cb
Evan Strohm <evanstrohm@gmail.com>
2016-10-10 18:14:28 -0400

    Edited 'login.py' to finally have functionality, including interaction with the database and successful output of dynamically generated HTML. Added script 'logout.py' and a button to log out a user. Note that 'logout.py' is not yet functional

--- www/cgi-bin/login.py
+++ www/cgi-bin/login.py
@@ -3,133 +3,109 @@
 import cgi
 import cgitb
 import mysql.connector
+import Cookie
+import os
 
 cgitb.enable()
 
+cook_str = os.environ.get('HTTP_COOKIE')
 print 'Content-type: text/html'
-print
+header = ''
+content = ''
+if not cook_str :
+	form = cgi.FieldStorage()
 
-form = cgi.FieldStorage()
+	username = form["username"].value
+	password = form["password"].value
 
-username = form["username"].value
-password = form["password"].value
+	conn = mysql.connector.connect(user='root', password='mysql', database='Thrones_Database')
+	cursor = conn.cursor()
 
-conn = mysql.connector.connect(user='root', password='mysql', database='Thrones_Database')	#This is probably a garbage line
-cursor = conn.cursor()
+	query = 'SELECT COUNT(*) FROM User WHERE username=%s AND password=%s'
 
-query = 'SELECT * FROM User WHERE username=%s AND password=%s'
+	cursor.execute(query, (username, password))
 
-cursor.execute(query, (username, password))
-
-if True :#TODO : find how to determine if cursor contains a row	
-	# TODO somehow log them in
-	# use a cookie?
-	# Then bring them to a dummy "You have been logged in!" page
-	print '''<!doctype html>
-	<html>
-		<head>
-			<title>CSC 210 Project</title>
-			<link rel="stylesheet" type="text/css" href="../css/nav-bar.css">
-			<link rel="stylesheet" type="text/css" href="../css/styles.css">
-		</head>
-		<body>
-			<header> <!-- All Top-level .html files should have exactly the same header contents -->
-				<div class="login-top">
+	row = cursor.fetchone()
+	
+	if row == 1 :
+		header = 'Set-Cookie: logged_in=' + username + '; path=/'
+		content += '<h1>Congratulations, ' + username + ', you have successfully logged in</h1>'
+	else :
+		content += '''<h1>Incorrect username or password</h1>
 					<form method="POST" action="../cgi-bin/login.py"">
-						Login: 
-						Username: <input type="text" name="username" required/> 
-						Password: <input type="text" name="password" required/>TODO: Script
-						<input type="submit" value="Log in!"/>
-					</form>
-					<a href="../html-pages/create-account.html">Create Account</a>
-				</div>
-			</header>
-			<nav><!-- All Top-level .html files should have exactly the same header contents -->
-				<ul>
-					<li>
-						<a class="current" href="../index.html">Home</a>
-					</li>
-					<li>
-						<a href="../forum.html">Forum</a>
-					</li>	
-					<li>
-						<a href="../wiki.html">Wiki</a>
-					</li>
-					<li>
-						<a href="../about.html">About</a>
-					</li>
-					<li>
-						<a href="../user-account.html">User Account</a>
-					</li>
-				</ul>
-			</nav>
-			<article>
-				<br><br><br><br>
-				<h1>Congratulations, {}, you have successfully logged in</h1>
-			</article>
-			<aside>
-			
-			</aside>
-			<footer>
-			
-			</footer>
-			
-		</body>
-	</html>'''.format(username)
+							Username: <input type="text" name="username" required/> <br>
+							Password: <input type="password" name="password" required/> <br>
+							<input type="submit" value="Log in!"/>
+					</form>'''
+elif 'logged_in' in cook_str :
+	cookie = Cookie.SimpleCookie(cook_str)
+	content += '<h1>You are already logged in, ' + cookie['logged_in'].value + '''!</h1>
+					<form method="POST" action="../cgi-bin/logout.py"">
+						<input type="submit" value="Log out"/>
+					</form>'''
 else :
-	# Bring them to a dummy "Username and/or password incorrect" page
-	print '''<!doctype html>
-	<html>
-		<head>
-			<title>CSC 210 Project</title>
-			<link rel="stylesheet" type="text/css" href="../css/nav-bar.css">
-			<link rel="stylesheet" type="text/css" href="../css/styles.css">
-		</head>
-		<body>
-			<header> <!-- All Top-level .html files should have exactly the same header contents -->
-				<div class="login-top">
-					<form method="POST" action="../cgi-bin/login.py"">
-						Login: 
-						Username: <input type="text" name="username" required/> 
-						Password: <input type="text" name="password" required/>TODO: Script
-						<input type="submit" value="Log in!"/>
-					</form>
-					<a href="../html-pages/create-account.html">Create Account</a>
-				</div>
-			</header>
-			<nav><!-- All Top-level .html files should have exactly the same header contents -->
-				<ul>
-					<li>
-						<a class="current" href="../index.html">Home</a>
-					</li>
-					<li>
-						<a href="../forum.html">Forum</a>
-					</li>	
-					<li>
-						<a href="../wiki.html">Wiki</a>
-					</li>
-					<li>
-						<a href="../about.html">About</a>
-					</li>
-					<li>
-						<a href="../user-account.html">User Account</a>
-					</li>
-				</ul>
-			</nav>
-			<article>
-				<br><br><br><br>
-				<h1>Incorrect username or password</h1>
-				<form method="POST" action="../cgi-bin/login.py"">
-						Username: <input type="text" name="username" required/> <br>
-						Password: <input type="text" name="password" required/>
-				</form>
-			</article>
-			<aside>
-			
-			</aside>
-			<footer>
-			
-			</footer>
-			
-		</body>
-	</html>'''
+	content = '''<h1>Somehow, you have a malformed cookie :</h1>
+				<ul>'''
+	for key in cookie :
+		content += '<li>' + key + ':' + cookie[key].value + '</li>'
+	content += '</ul>'
+
+print header
+print
+print '''Content-type: text/html
+Set-Cookie: logged_in={}; path=/
+
+		<!doctype html>
+		<html>
+			<head>
+				<title>CSC 210 Project</title>
+				<link rel="stylesheet" type="text/css" href="../css/nav-bar.css">
+				<link rel="stylesheet" type="text/css" href="../css/styles.css">
+			</head>
+			<body>
+				<header> <!-- All Top-level .html files should have exactly the same header contents -->
+					<div class="login-top">
+						<form method="POST" action="../cgi-bin/login.py"">
+							Login: 
+							Username: <input type="text" name="username" required/> 
+							Password: <input type="password" name="password" required/>
+							<input type="submit" value="Log in!"/>
+						</form>
+						<form method="POST" action="../cgi-bin/logout.py"">
+							<input type="submit" value="Log out"/>
+						</form>
+						<a href="../html-pages/create-account.html">Create Account</a>
+					</div>
+				</header>
+				<nav><!-- All Top-level .html files should have exactly the same header contents -->
+					<ul>
+						<li>
+							<a class="current" href="../index.html">Home</a>
+						</li>
+						<li>
+							<a href="../forum.html">Forum</a>
+						</li>	
+						<li>
+							<a href="../wiki.html">Wiki</a>
+						</li>
+						<li>
+							<a href="../about.html">About</a>
+						</li>
+						<li>
+							<a href="../user-account.html">User Account</a>
+						</li>
+					</ul>
+				</nav>
+				<article>
+					<br><br><br><br>'''
+print content
+print '''</article>
+				<aside>
+				
+				</aside>
+				<footer>
+				
+				</footer>
+				
+			</body>
+		</html>'''
