7e14ae74297e12c474867da06f251af6d96524f3
cperkin3 <chrisperkins.cp@gmail.com>
2016-11-19 15:04:01 -0500

    moving forum home page to forum folder

--- www/index.php
+++ www/index.php
@@ -12,7 +12,7 @@
 					<a class="current" href="index.php">Home</a>
 				</li>
 				<li>
-					<a href="forum.php">Forum</a>
+					<a href="forum/forum.php">Forum</a>
 				</li>	
 				<li>
 					<a href="wiki.php">Wiki</a>
