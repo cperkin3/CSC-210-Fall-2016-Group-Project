b54498e68e764d7d535ee0971f036cac47983d18
cperkin3 <chrisperkins.cp@gmail.com>
2016-11-19 15:28:01 -0500

    Removing include links

--- www/create-account.php
+++ www/create-account.php
@@ -2,10 +2,36 @@
 <html>
 	<head>
 		<title>CSC 210 Project</title>
-		<?php include("includes/imports.html");?>
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="css/nav-bar.css">
+		<link rel="stylesheet" type="text/css" href="css/login-bar.css">
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<!-- JavaScript --> 
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="js/header.js"></script>
 	</head>
 	<body>
-		<?php include("includes/header.html");?>
+		<header> 
+			<div class="login-top">
+				<div id="logged-in">
+					<span id="welcome-name"></span>
+					<form method="POST" action="cgi-bin/logout.py">
+						<input type="submit" value="Log out"/>
+					</form>
+				</div>
+				<div id="logged-out">
+					<form method="POST" action="cgi-bin/login.py">
+						Username: <input type="text" name="username" required/> 
+						Password: <input type="password" name="password" required/>
+						<input type="submit" value="Log in!"/>
+					</form>
+					<a href="create-account.php">Create Account</a>
+				</div>
+				<script type="text/javascript">
+					showHeader();
+				</script>
+			</div>
+		</header>
 		<nav><!-- All Top-level .html files should have exactly the same header contents -->
 			<ul>
 				<li>
